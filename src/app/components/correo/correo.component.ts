840da38b72ef46dc0b12819012becba9a561ef9f
Jose <email>
2020-03-26 14:02:42 +0100

    Creada lista de correo

---
+++ src/app/components/correo/correo.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-correo',
+  templateUrl: './correo.component.html',
+  styleUrls: ['./correo.component.scss']
+})
+export class CorreoComponent implements OnInit {
+
+  correo:any;
+
+  constructor() { 
+    this.correo={
+      titulo: "Título del email",
+      cuerpo: "Cuerpo del email, Cuerpo del email, Cuerpo del email, Cuerpo del email, Cuerpo del email, Cuerpo del email, Cuerpo del email",
+      emisor: "correoEmisor@openWebinar.inv",
+      destinatario: "correoReceptor@openWebinar.inv"
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+}
